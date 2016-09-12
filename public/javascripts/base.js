827f184d396045a8320595097a86a923148f7768
candyhh <17764576286@163.com>
2016-09-12 18:02:17 +0800

    wechat

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -44,17 +44,7 @@ wx.config({
 });
 
 wx.ready(function() {
-  wx.onMenuShareAppMessage({
-    title : '天哪！我竟然闯关失败',
-    desc : '在下月饼之旅闯关失败，大侠可否助我一臂之力',
-    imgUrl : 'http://a4.qpic.cn/psb?/V12r8I4D1MpPWh/VDPoxJ7pB7WUrgvqv5xGFZ1cMKqT*QT1gLwqBnuxF7k!/b/dG8BAAAAAAAA&ek=1&kp=1&pt=0&bo=9QH1AfUB9QEDCC0!&sce=0-12-12&rf=viewer_311',
-    link : '<%=browserUrl%>' + '<%=question%>',
-    success:function () {
-      var wrong = '<%=wrong%>';
-      alert(wrong+'?flag=1');
-      window.location.href=wrong+'?flag=1';
-    }
-  });
+  wx.onMenuShareAppMessage(shareData);
   wx.onMenuShareTimeline(shareData);
   wx.hideMenuItems({
     menuList: ['menuItem:originPage', 'menuItem:openWithQQBrowser',
@@ -62,12 +52,9 @@ wx.ready(function() {
     ],
   });
 });
-
-
-    // wx.error(function(res) {
-    //   alert(res.errMsg);
-    // });
-    // wx.success(function () {
-    //   alert('hello');
-    //   // window.location.href='<%=wrong%>'+'?flag=1';
-    // })
+wx.error(function(res) {
+  alert(res.errMsg);
+});
+wx.success(function () {
+  alert('hhhhh');
+})
