32e77b681dcdfbb341c79ac2f8eb4f4a68c886e5
candyhh <17764576286@163.com>
2016-09-12 17:52:20 +0800

    wechat

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -45,9 +45,12 @@ wx.config({
 
 wx.ready(function() {
   wx.onMenuShareAppMessage({
-    shareData,
+    title : '天哪！我竟然闯关失败',
+    desc : '在下月饼之旅闯关失败，大侠可否助我一臂之力',
+    imgUrl : 'http://a4.qpic.cn/psb?/V12r8I4D1MpPWh/VDPoxJ7pB7WUrgvqv5xGFZ1cMKqT*QT1gLwqBnuxF7k!/b/dG8BAAAAAAAA&ek=1&kp=1&pt=0&bo=9QH1AfUB9QEDCC0!&sce=0-12-12&rf=viewer_311',
+    link : '<%=browserUrl%>' + '<%=question%>',
     success:function () {
-      alert('hhhhhh');
+      window.location.href='<%=wrong%>'+'?flag=1';
     }
   });
   wx.onMenuShareTimeline(shareData);
