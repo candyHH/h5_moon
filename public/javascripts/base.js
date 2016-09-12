6bcf1ade14c2e7e4042c8e05bb4fae32fdb90f6b
su zhao <suzhao@sumatoMacBook-Air.local>
2016-09-12 18:27:03 +0800

    ;ll

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -44,20 +44,7 @@ wx.config({
 });
 
 wx.ready(function() {
-  wx.onMenuShareAppMessage({
-    title : '天哪！我竟然闯关失败',
-    desc : '在下月饼之旅闯关失败，大侠可否助我一臂之力',
-    imgUrl : 'http://a4.qpic.cn/psb?/V12r8I4D1MpPWh/VDPoxJ7pB7WUrgvqv5xGFZ1cMKqT*QT1gLwqBnuxF7k!/b/dG8BAAAAAAAA&ek=1&kp=1&pt=0&bo=9QH1AfUB9QEDCC0!&sce=0-12-12&rf=viewer_311',
-    link : 'https://www.baidu.com',
-    // link : '<%=browserUrl%>' + '<%=question%>',
-
-    success:function (res) {
-      // var wrong = '<%=wrong%>';
-      // alert(wrong+'?flag=1');
-      alert('hello');
-      window.location.href=wrong+'?flag=1';
-    }
-  });
+  wx.onMenuShareAppMessage(shareData);
   wx.onMenuShareTimeline(shareData);
   wx.hideMenuItems({
     menuList: ['menuItem:originPage', 'menuItem:openWithQQBrowser',
@@ -65,9 +52,9 @@ wx.ready(function() {
     ],
   });
 });
-wx.error(function(res) {
-  alert(res.errMsg);
-});
-wx.success(function (res) {
-  alert('hhhhh');
-})
+// wx.error(function(res) {
+//   alert(res.errMsg);
+// });
+// wx.success(function (res) {
+//   alert('hhhhh');
+// })
