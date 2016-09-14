be53036b7b8821676b8c4ee500ea504bf0d83ee9
Durling <878877528@qq.com>
2016-09-14 20:09:31 +0800

    更新

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ logger_error.error('---app.js---');
 
 var app = express();
 
+
+// logger.info("=== this is log from app.js ===");
+process.on('uncaughtException', function (err) {
+    logger_error.error('=== CAUGHT EXCEPTION ===', err);
+});
+function haltOnTimedout(req, res, next){
+  logger.info('=== HALTONTIMEOUT REQ ===',req);
+  if (!req.timedout) next();
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -47,17 +58,17 @@ if (app.get('env') === 'development'){
   //开发环境
 	global.baseURL = 'http://dev.lalocal.cn:8080';
   global.wechatURL = 'http://node.lalocal.cn';
-  global.browserURL = 'https://dev.lalocal.cn/wechat';//浏览器实际url
+  global.browserURL = 'https://dev.lalocal.cn/wechat/h5Mooncake';//浏览器实际url
 }else if (app.get('env') === 'production'){
 	// 生产环境
 	global.baseURL = 'http://10.117.198.127:8080';
   global.wechatURL = 'http://node.lalocal.cn';
-  global.browserURL = 'https://h5.lalocal.cn';
+  global.browserURL = 'https://h5.lalocal.cn/h5Mooncake';
 }else if (app.get('env') === 'localhost'){
 	// 本地
 	global.baseURL = 'http://dev.lalocal.cn:8080';
   global.wechatURL = 'http://node.lalocal.cn';
-  global.browserURL = 'http://192.168.10.222';
+  global.browserURL = 'http://192.168.10.222/h5Mooncake';
 }else{
 	global.baseURL = 'http://dev.lalocal.cn:8080';
 }
@@ -67,6 +78,7 @@ if (app.get('env') === 'development'){
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   logger_error.error(err);
+  logger_error.error('222222');
 
   res.render('error', {
     message: err.message,
